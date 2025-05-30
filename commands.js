711c2b8bd8df3c95b44ef0b131fb9804797f24d5
MistbornOne <iankwatkins@gmail.com>
2025-05-30 15:18:54 -0400

    init commit

---
+++ commands.js
@@ -0,0 +1,86 @@
+// var password = "schfiftyfive";
+var linkedin = "https://www.linkedin.com/in/ian-watkins-5000a712a/";
+var github = "https://github.com/MistbornOne/";
+var email = "mailto:info@ianwatkins.dev";
+
+about = [
+  "<br>",
+  "Hey, I'm Ian 👋🏼",
+  "I'm the dad of 5 humans and 3 dogs who lives in Knoxville, TN",
+  "and loves the outdoors, a good cup of coffee and a better book.",
+  "I am a follower of Jesus.",
+  "Major nerd (just ask my wife)",
+  "and a sales executive who loves to build software.",
+  "I'm an ex-professional chef and BBQ enthusiast.",
+  "I am a creative at heart and storyteller by nature.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "That is a good question friend... who are you indeed?",
+  "<br>",
+];
+
+social = [
+  "<br>",
+
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/ian-watkins' +
+    "</a>",
+
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/MistbornOne' +
+    "</a>",
+  "<br>",
+];
+
+/* secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];*/
+
+projects = [
+  "<br>",
+  "My projects are available on <a href='https://github.com/MistbornOne' target='_blank'>GitHub</a> and <a href='https://ianwatkins.dev/projects/' target='_blank'>my personal website</a>.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Who is Ian?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         My socials',
+  '<span class="command">projects</span>       My coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           Display this help',
+  '<span class="command">email</span>          Reach out via email',
+  '<span class="command">clear</span>          Wipe the terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">games</span>          Play terminal games',
+  '<span class="command">story</span>          Be part of a story...',
+  // '<span class="command">secret</span>         Find the password',
+
+  "<br>",
+];
+
+banner = [
+  '<span class="index">©Ian Watkins. All rights reserved.</span>',
+  "<br>",
+  "██╗ █████╗ ███╗   ██╗    ██╗    ██╗ █████╗ ████████╗██╗  ██╗██╗███╗   ██╗███████╗",
+  "██║██╔══██╗████╗  ██║    ██║    ██║██╔══██╗╚══██╔══╝██║ ██╔╝██║████╗  ██║██╔════╝",
+  "██║███████║██╔██╗ ██║    ██║ █╗ ██║███████║   ██║   █████╔╝ ██║██╔██╗ ██║███████╗",
+  "██║██╔══██║██║╚██╗██║    ██║███╗██║██╔══██║   ██║   ██╔═██╗ ██║██║╚██╗██║╚════██║",
+  "██║██║  ██║██║ ╚████║    ╚███╔███╔╝██║  ██║   ██║   ██║  ██╗██║██║ ╚████║███████║",
+  "╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝     ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝",
+  "                                                                                 ",
+  '<span class="color2">This terminal is interactive:</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
+
+games = ["<br>", "Games are coming soon...", "<br>"];
+
+story = ["<br>", "Stories are coming soon...", "<br>"];
